09ea342f3cc246e0d9b0125164f9f9930b63e2cf
Tae Yoon Kim <the.thinkty@gmail.com>
2021-11-22 21:29:41 +0900

    Added template for fetching sensor data

--- main.go
+++ main.go
@@ -1,14 +1,51 @@
 package main
 
 import (
-	"fmt"
 	"log"
 	"net/http"
 )
 
-func main() {
-	fmt.Println("Start")
-	http.Handle("/", http.FileServer(http.Dir("./"))) // TODO: only allow index.html
+// BTRequest is the data structure used for sending queries from the server to
+// the bluetooth device.
+// Target is the name of the bluetooth device.
+// Command specifies which feature of the bluetooth device it should execute.
+// Value is an optional parameter for specific command operations (ex: setting
+// the status of the LED)
+type BTRequest struct {
+	target  string
+	command byte
+	value   byte
+}
 
-	log.Fatal(http.ListenAndServe(":8080", nil))
+// BTResponse is the data structure used for receiving responses from the
+// bluetooth device to the server
+// Target specifies which bluetooth device it is coming from.
+// Key is the name of the feature the bluetooth device is reporting (ex:
+// TEMPERATURE, LED)
+// Value is the actual value corresponding to the key (ex: actual temperature
+// values, LED status on/off)
+type BTResponse struct {
+	target string
+	key    string
+	value  string
+}
+
+func main() {
+	http.Handle("/", http.FileServer(http.Dir("./public")))
+	http.HandleFunc("/data", getData)
+
+	log.Fatal(http.ListenAndServe("localhost:8080", nil))
+}
+
+// Retrieve all data from the database
+func getData(rw http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodGet {
+		http.Error(rw, "Only GET method is supported", http.StatusBadRequest)
+		return
+	}
+
+	// TODO: request sensor values from the database
+
+	rw.Header().Set("Content-Type", "application/json")
+
 }
