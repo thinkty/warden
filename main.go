242cdaae71a8faa20b96ced75377d3930bfac9a7
Tae Yoon Kim <the.thinkty@gmail.com>
2021-11-29 00:03:50 +0900

    Updated table creation query and added db query handler

--- main.go
+++ main.go
@@ -36,9 +36,25 @@ type BTResponse struct {
 	value  string
 }
 
+// DBRow is the structure of the sensor table in the database.
+type DBRow struct {
+	id int
+	date string
+	beacon string
+	name string
+	record_type int
+	record string
+}
+
+// Parse DBItem into string format for logging purpose
+func (item DBRow) String() string {
+	return fmt.Sprintf("\tID:\t%d\n\tDate:\t%s\n\tBeacon:\t%s\n\tName:\t%s\n\tRecord-Type:\t%d\n\tRecord:%s\n", item.id, item.date, item.beacon, item.name, item.record_type, item.record)
+}
+
 func main() {
 	initDB()
 
+	// TODO: might be better to implement a middleware to log and handle errors
 	http.Handle("/", http.FileServer(http.Dir("./public")))
 	http.HandleFunc("/ok", getHealth)
 	http.HandleFunc("/data", getData)
@@ -60,21 +76,22 @@ func initDB() {
 
 	db, err := sql.Open("sqlite3", "./sensor.db")
 	if err != nil {
-		log.Fatal(err)
+		log.Panic(err)
 	}
 	defer db.Close()
 
+	// Create table
 	query := `CREATE TABLE sensor
 	(
 		id INTEGER PRIMARY KEY,
-		date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+		date DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
 		beacon TEXT NOT NULL,
 		name TEXT NOT NULL,
 		record_type INTEGER NOT NULL,
 		record BLOB
 	)`
 	if _, err = db.Exec(query); err != nil {
-		log.Fatalf("%q: %s\n", err, query)
+		log.Panicf("%q: %s\n", err, query)
 	}
 }
 
@@ -93,7 +110,35 @@ func getData(rw http.ResponseWriter, r *http.Request) {
 	}
 
 	// TODO: request sensor values from the database
+	db, err := sql.Open("sqlite3", "./sensor.db")
+	if err != nil {
+		http.Error(rw, "Connection to database failed", http.StatusInternalServerError)
+		log.Panic(err)
+		return
+	}
+	defer db.Close()
 
+	rows, err := db.Query("SELECT * FROM sensor")
+	if err != nil {
+		// On query failure, report to the client but don't panic
+		http.Error(rw, "Query failed", http.StatusInternalServerError)
+		log.Print(err)
+		return
+	}
+	defer rows.Close()
+
+	data := make([]DBRow, 0)
+	for rows.Next() {
+		var row DBRow
+		if err := rows.Scan(&row.id, &row.date, &row.beacon, &row.name, &row.record_type, &row.record); err != nil {
+			http.Error(rw, "Row scan failed", http.StatusInternalServerError)
+			log.Print(err)
+			return
+		}
+		data = append(data, row)
+	}
+
+	fmt.Println(data)
 	// rw.Header().Set("Content-Type", "application/json")
-	fmt.Fprint(rw, "OK!")
+	rw.WriteHeader(http.StatusOK) // TODO: send content
 }
