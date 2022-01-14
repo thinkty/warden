ae37e39a2d8831c97874e76558674c0eaf6b0751
thinkty <thinkty@naver.com>
2022-01-14 16:41:04 +0900

    Parsing and printing UserActions on click

--- web/src/components/Item.tsx
+++ web/src/components/Item.tsx
@@ -1,17 +1,17 @@
 import React from 'react';
 
-type RecordContent = {
-  String: string;
-  Valid: boolean;
-}
+type UserAction = {
+  label: string;
+  value: string;
+};
 
 export type SensorRecord = {
   Id: number;
   Date: Date;
   Beacon: string;
   Name: string;
-  RecordType: number;
-  Record: RecordContent;
+  Record: { String: string, Valid: boolean };
+  UserActions: { String: string, Valid: boolean };
 };
 
 type Props = {
@@ -24,7 +24,10 @@ export const Item = ({ records }: Props): JSX.Element => {
 
   return (
     <div
-      onClick={() => { console.log(records) }}
+      onClick={() => {
+        console.log(records); // TODO:
+        console.log(parseUserActions(records[0].UserActions.String)); // TODO:
+      }}
       style={{
         display: 'flex',
         flexDirection: 'column',
@@ -75,8 +78,47 @@ export const Item = ({ records }: Props): JSX.Element => {
         style={{
 
         }}
-      />
+      >
+      </div>
     </div>
   );
 }
 Item.defaultProps = defaultProps;
+
+/**
+ * Parse raw user-actions retrieved from the database. The argument is separated
+ * into each user-action by commas and each user-action is a colon separated
+ * data structure similar to a dictionary. The left side is the label which will
+ * be displayed to the user and the right side is the value that will be sent to
+ * the server when triggered. If the user value is provided, it will be sent to
+ * the server instead of the default value.
+ */
+function parseUserActions(rawUserActions: string): UserAction[] {
+  const impureUserActions = rawUserActions.split(",").map((rawAction) => {
+    const dividerIndex = rawAction.indexOf(":");
+
+    // Error
+    if (dividerIndex === -1 || dividerIndex === rawAction.length - 1) {
+      return null;
+    }
+
+    const action: UserAction = {
+      label: rawAction.substring(0, dividerIndex),
+      value: rawAction.substring(dividerIndex + 1)
+    };
+
+    return action;
+  });
+
+  const userActions: UserAction[] = [];
+
+  for (let i = 0; i < impureUserActions.length; i++) {
+    const action = impureUserActions[i];
+    if (action != null) {
+      userActions.push(action);
+    }
+  }
+
+  // The code above is pretty shitty, but I will let it stay bc it hurts my ego.
+  return userActions;
+}
