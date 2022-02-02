bcf116bccba2ded9dc2102a0c25c442644e840ff
Tae Yoon Kim <the.thinkty@gmail.com>
2022-02-02 22:14:03 +0900

    Writing back to broadcast maker

--- cmd/ward/main.go
+++ cmd/ward/main.go
@@ -24,4 +24,12 @@ func main() {
 	}
 
 	log.Printf("%s sent this: %s\n", addr, buf[:n])
+
+	message := []byte("warden")
+	n, err = conn.WriteTo(message, addr)
+	if err != nil {
+		log.Panic(err)
+	}
+
+	log.Printf("Ward sent %d bytes to %s", n, addr)
 }
